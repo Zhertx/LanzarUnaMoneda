1e357390375cd360fe74afd09a13d450c7fc40dd
Zhertx <133716156+Zhertx@users.noreply.github.com>
2025-05-11 22:18:52 -0400

    Delete Pagina web/moneda directory

--- Pagina web/moneda/moneda/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from lanzamiento.views import tirar_moneda
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', tirar_moneda, name='tirar_moneda'),
-]
