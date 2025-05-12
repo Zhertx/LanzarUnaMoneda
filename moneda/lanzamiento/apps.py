cceb02e3093f3f38b74d4e4c2b52ca3c81f7915d
Zhertx <133716156+Zhertx@users.noreply.github.com>
2025-05-11 22:20:36 -0400

    Delete moneda directory

--- moneda/lanzamiento/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LanzamientoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lanzamiento'
--- moneda/lanzamiento/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-import random
-
-def tirar_moneda(request):
-    resultado = random.choice(['Cara', 'Cruz'])
-    return render(request, 'index.html', {'resultado': resultado})
